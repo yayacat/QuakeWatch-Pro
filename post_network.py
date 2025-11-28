8b1fdbd8a4d6ae78f893075bfcb03fe1617caaf0
夜夜 <vcde0000@gmail.com>
2025-11-28 18:40:48 +0800

    fix: post_network

--- post_network.py
+++ post_network.py
@@ -1,21 +1,21 @@
 import requests
 import argparse
 
-parser = argparse.ArgumentParser(description='傳送連接到WiFi網絡資訊到ES-Net裝置上。')
-parser.add_argument('ssid', type=str, help='連接到WiFi網絡的SSID。')
-parser.add_argument('password', type=str, help='連接到WiFi網絡的密碼。')
+parser = argparse.ArgumentParser(description='傳送連接到WiFi網路資訊到ES-Net裝置上。')
+parser.add_argument('ssid', type=str, help='連接到WiFi網路的SSID。')
+parser.add_argument('password', type=str, help='連接到WiFi網路的密碼。')
 args = parser.parse_args()
 
 ssid = args.ssid
 password = args.password
 
 if not ssid or not password:
-    print("必須提供連接到WiFi網絡的SSID和密碼。請使用--ssid和--password參數提供它們或直接在執行檔後方依序輸入連接到WiFi網絡的SSID和密碼。")
+    print("必須提供連接到WiFi網路的SSID和密碼。請使用--ssid和--password參數提供它們或直接在執行檔後方依序輸入連接到WiFi網路的SSID和密碼。")
 else:
     print("即將傳送以下WiFi連接資訊到ES-Net裝置：")
     print(f"SSID: {ssid}")
     print(f"密碼: {password}")
-    confirm = input("請確認SSID和密碼是否正確（Y/N）: ")
+    confirm = input("請確認SSID和密碼是否正確（y/N）: ")
     if confirm.lower() != 'y':
         print("操作已取消。")
         exit()
@@ -32,8 +32,6 @@ else:
         print(response.text)
     except requests.exceptions.ConnectTimeout:
         print("連線超時。無法連接到 'http://192.168.4.1'。請檢查：")
-        print("1. 您是否已連接到目標設備的WiFi網絡？")
-        print("2. 設備的IP地址是否正確？")
-        print("3. 是否有防火牆阻止了連線？")
+        print("1. 您是否已連接到目標設備的WiFi網路？")
     except requests.exceptions.RequestException as e:
         print(f"發生未預期的錯誤：{e}")
